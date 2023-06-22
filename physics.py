86eaf14d2533700ce33e9528f93338fbcd7d226c
Zac-Milioli <zacmilioli@gmail.com>
2023-06-22 16:45:42 -0300

    Adicionada parcialmente função de seleção de form

--- physics.py
+++ physics.py
@@ -27,9 +27,29 @@ def energia_potencial(m, g, h):
     return e
 
 def energia_cinetica(m, v):
-    e = (m*v^2)/2
+    e = (m*(v^2))/2
     return e
 
 def energia_mecanica(m, g, h, v):
     e = energia_potencial(m, g, h) + energia_cinetica(m, v)
     return e
+
+def select_option(opt):
+    if opt == 1:
+        x0 = int(input('\nInsira a posição inicial: '))
+        v = int(input('\nInsira a velocidade: '))
+        t = int(input('\nInsira o a variação do tempo: '))
+        result = mru(x0, v, t)
+    elif opt == 2:
+        v0 = int(input('\nInsira a velocidade inicial: '))
+        a = int(input('\nInsira a aceleração: '))
+        t = int(input('\nInsira a variação do tempo: '))
+        result = muv(v0, a, t)
+    elif opt == 3:
+        v0 = int(input('\nInsira a velocidade inicial: '))
+        a = int(input('\nInsira a aceleração: '))
+        delta_x = int(input('\nInsira a variação da posição: '))
+        result = torricelli(v0, a, delta_x)
+    else:
+        return '\nObrigado por usar o programa de cálculos físicos do Zac!'
+    return result

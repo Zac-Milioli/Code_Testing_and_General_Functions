3be3b65382fa50850dd4873bc127be6fb7930d54
Zac_Milioli_viaNotebook <zacmilioli@gmail.com>
2023-06-22 22:34:42 -0300

    Refatorações e correções

--- physics.py
+++ physics.py
@@ -37,39 +37,39 @@ def energia_mecanica(m, g, h, v):
 def menu(execute):
     opt = int(input(interface))
     if opt == 1:
-        x0 = int(input('\nInsira a posição inicial: '))
-        v = int(input('\nInsira a velocidade: '))
-        t = int(input('\nInsira o a variação do tempo: '))
+        x0 = float(input('\nInsira a posição inicial: '))
+        v = float(input('\nInsira a velocidade: '))
+        t = float(input('\nInsira o a variação do tempo: '))
         result = mru(x0, v, t)
     elif opt == 2:
-        v0 = int(input('\nInsira a velocidade inicial: '))
-        a = int(input('\nInsira a aceleração: '))
-        t = int(input('\nInsira a variação do tempo: '))
+        v0 = float(input('\nInsira a velocidade inicial: '))
+        a = float(input('\nInsira a aceleração: '))
+        t = float(input('\nInsira a variação do tempo: '))
         result = muv(v0, a, t)
     elif opt == 3:
-        v0 = int(input('\nInsira a velocidade inicial: '))
-        a = int(input('\nInsira a aceleração: '))
-        delta_x = int(input('\nInsira a variação da posição: '))
+        v0 = float(input('\nInsira a velocidade inicial: '))
+        a = float(input('\nInsira a aceleração: '))
+        delta_x = float(input('\nInsira a variação da posição: '))
         result = torricelli(v0, a, delta_x)
     elif opt == 4:
-        m = int(input('\nInsira a massa: '))
-        g = int(input('\nInsira um valor para a gravidade: '))
-        h = int(input('\nInsira a altura: '))
+        m = float(input('\nInsira a massa: '))
+        g = float(input('\nInsira um valor para a gravidade: '))
+        h = float(input('\nInsira a altura: '))
         result = energia_potencial(m, g, h)
     elif opt == 5:
-        m = int(input('\nInsira a massa'))
-        v = int(input('\nInsira a velocidade'))
+        m = float(input('\nInsira a massa'))
+        v = float(input('\nInsira a velocidade'))
         result = energia_cinetica(m, v)
     elif opt == 6:
-        m = int(input('\nInsira a massa: '))
-        g = int(input('\nInsira um valor para a gravidade: '))
-        h = int(input('\nInsira a altura: '))
-        v = int(input('\nInsira a velocidade'))
+        m = float(input('\nInsira a massa: '))
+        g = float(input('\nInsira um valor para a gravidade: '))
+        h = float(input('\nInsira a altura: '))
+        v = float(input('\nInsira a velocidade'))
         result = energia_mecanica(m, g, h, v)
     else:
         execute = False
-        return '\nObrigado por usar o programa de cálculos físicos do Zac!'
-    return result
+        return '\nObrigado por usar o programa de cálculos físicos automáticos!'
+    return f'\n-> {result}'
 
 interface = '''\nBem vindo ao programa de cálculos físicos automáticos!
 Escolha uma opção de fórmula:

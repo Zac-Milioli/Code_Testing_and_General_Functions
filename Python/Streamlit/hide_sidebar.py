a5bd0d0af592f194ece7fbcf26aeb7787a30e418
Zac-Milioli <zacmilioli@gmail.com>
2024-06-18 10:08:26 -0300

    hide sidebar

---
+++ Python/Streamlit/hide_sidebar.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed')
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+    [data-testid="stToolbarActions"] {
+        display: none
+    }
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+</style>
+""", unsafe_allow_html=True,)

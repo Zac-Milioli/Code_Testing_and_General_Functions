7347cbf564981ed1901b3a9e1eaf24f854d84845
Zac-Milioli <zacmilioli@gmail.com>
2024-06-26 14:03:22 -0300

    comparador funcional

--- Python/Pandas/Testes para o LabEEE/compare_dfs/comparer.py
+++ Python/Pandas/Testes para o LabEEE/compare_dfs/comparer.py
@@ -1,31 +1,35 @@
 import pandas as pd
+import os
+os.chdir(r"Python/Pandas/Testes para o LabEEE/compare_dfs")
 
-df_correct = pd.read_csv(r"compare_dfs/correct.csv")
-df_new = pd.read_csv(r"compare_dfs/new.csv")
+df_desktop = pd.read_csv(r"desktop.csv")
+df_webapp = pd.read_csv(r"webapp.csv")
 
 
-df_correct = df_correct.loc[df_correct['zone'] == "BWC"]
-df_new = df_new.loc[df_new['zone'] == "BWC"]
-df_correct =df_correct.drop(columns=['case', 'flux'])
-df_new = df_new.drop(columns=['case', 'flux'])
-print("\n\n\nCorrect:\n")
-print(df_correct)
-print("\n\n\nNew:\n")
-print(df_new)
+df_desktop = df_desktop.loc[df_desktop['zone'] == "SALA"]
+df_webapp = df_webapp.loc[df_webapp['zone'] == "SALA"]
+df_desktop =df_desktop.drop(columns=['case', 'flux'])
+df_webapp = df_webapp.drop(columns=['case', 'flux'])
+print("\n\n\nDesktop:\n")
+print(df_desktop)
+print(f"Size: {len(df_desktop)}")
+print("\n\n\nWebApp:\n")
+print(df_webapp)
+print(f"Size: {len(df_webapp)}")
 print("\n\n\n")
 
-merged_df = pd.merge(df_correct, df_new, on=["gains_losses", "zone", "heat_direction"], suffixes=('_correct', '_new'))
+merged_df = pd.merge(df_desktop, df_webapp, on=["gains_losses", "zone", "heat_direction"], suffixes=('_desktop', '_webapp'))
 
-merged_df['value_comparison'] = merged_df['value_correct'] == merged_df['value_new']
+merged_df['value_comparison'] = merged_df['value_desktop'] == merged_df['value_webapp']
 
 false_df = merged_df[merged_df["value_comparison"] == False]
 
 true_df = merged_df[merged_df["value_comparison"] == True]
 
 print("\n\n\nComparison Results:\n")
-print(merged_df[['gains_losses', 'heat_direction', 'zone', 'value_correct', 'value_new', 'HEI_correct', 'HEI_new', 'value_comparison']])
+print(merged_df[['gains_losses', 'heat_direction', 'zone', 'value_desktop', 'value_webapp', 'HEI_desktop', 'HEI_webapp', 'value_comparison']])
 print("\n\n\nFalse values:\n")
-print(false_df[['gains_losses', 'heat_direction', 'zone', 'value_correct', 'value_new', 'HEI_correct', 'HEI_new', 'value_comparison']])
+print(false_df[['gains_losses', 'heat_direction', 'zone', 'value_desktop', 'value_webapp', 'HEI_desktop', 'HEI_webapp', 'value_comparison']])
 print("\n\n\nTrue values:\n")
-print(true_df[['gains_losses', 'heat_direction', 'zone', 'value_correct', 'value_new', 'HEI_correct', 'HEI_new', 'value_comparison']])
+print(true_df[['gains_losses', 'heat_direction', 'zone', 'value_desktop', 'value_webapp', 'HEI_desktop', 'HEI_webapp', 'value_comparison']])
 print("\n\n\n")

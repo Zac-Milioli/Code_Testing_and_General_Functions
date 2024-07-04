f444dfc7c64e1f8e85ef9110b8d2dfd1bc3fce74
Zac-Milioli <zacmilioli@gmail.com>
2024-07-04 11:39:21 -0300

    conexão com gsheets e streamlit

---
+++ Python/Streamlit/google_sheets_conn.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import gspread
+from google.oauth2.service_account import Credentials
+import json
+
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets["credentials"]
+credentials = {"example": "No auth"}
+
+with open("credentials.json", "w") as file:
+    json.dump(credentials, file, indent=2)
+
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file(r"credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+# ESTA VARIÁVEL É SENSÍVEL E NA APLICAÇÃO REAL DEVE SER ADICIONADA AO ENV DO STREAMLIT E ACESSADA ATRAVÉS DE st.secrets["sheet_id"]
+sheet_id = "example id"
+workbook = client.open_by_key(sheet_id)
+worksheet = workbook.worksheet('user')
+
+conn = st.connection("gsheets", type=GSheetsConnection, ttl=5)
+data = conn.read(worksheet='user')
+st.dataframe(data, use_container_width=True)
+
+name = st.text_input("name")
+id_ = st.text_input("id")
+text = st.text_input("text")
+
+col1, col2 = st.columns(2)
+if col1.button("Adicionar linha nova", use_container_width=True):
+    worksheet.append_row([name, id_, text])
+if col2.button("Recarregar a página", use_container_width=True):
+    st.rerun()

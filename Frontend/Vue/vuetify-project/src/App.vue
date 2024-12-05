ab03f50afdac2d0a2aa7383936e1f8e2bf92e8bb
Zac Milioli <zac.milioli@agcapital.com.br>
2024-12-05 16:06:52 -0300

    update: adição do projeto vue inicializado

---
+++ Frontend/Vue/vuetify-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app theme="dark">
+    <VAppBar color="blue" title="Aplicativo do Zac"></VAppBar>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+import { VSlideGroup } from 'vuetify/components';
+
+</script>

f960aa93102bbbc6270fae488c9182fb087e8caa
Zac Milioli <zac.milioli@agcapital.com.br>
2024-12-06 13:44:21 -0300

    caixa

--- Frontend/Vue/vuetify-project/src/App.vue
+++ Frontend/Vue/vuetify-project/src/App.vue
@@ -2,9 +2,16 @@
   <v-app theme="dark">
     <VAppBar color="blue" title="Aplicativo do Zac"></VAppBar>
     <v-main>
+      <VContainer>
+        <div
+          style="width: 100px; height: 100px;"
+          class="border rounded"
+          >
+          Teste
+        </div>
+      </VContainer>
       <HelloWorld />
     </v-main>
-
     <AppFooter />
   </v-app>
 </template>
